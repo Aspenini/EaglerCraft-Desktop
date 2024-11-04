dd5e158b6e01f5029a702edaede2c59a0e6c5871
Aspenini <bobfeltner123@gmail.com>
2024-11-04 13:30:49 -0500

    inital commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // Optional, if you use a preload script
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile('EaglercraftX.html'); // Load EaglercraftX.html here
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
